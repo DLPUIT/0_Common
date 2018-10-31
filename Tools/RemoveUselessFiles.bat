ef3932581362757339032bb9248d8ab7da311c76
Hendry <imnista@gmail.com>
2018-10-31 13:30:33 +0800

    提供一个清理无用编译文件的脚本

---
+++ Tools/RemoveUselessFiles.bat
@@ -0,0 +1,16 @@
+
+REM start to clean code folder
+REM Hendry nap7.com
+
+echo Start to clean all code folder
+
+@echo off
+
+@for /d /r %%c in (obj) do @if exist "%%c" (@rd /s /q "%%c" & echo Delete %%c)
+@for /d /r %%c in (bin) do @if exist "%%c" (@rd /s /q "%%c" & echo Delete %%c)
+@for /d /r %%c in (packages) do @if exist "%%c" (@rd /s /q "%%c" & echo Delete %%c)
+@for /d /r %%c in (.vs) do @if exist "%%c" (@rd /s /q "%%c" & echo Delete %%c)
+
+echo Done.
+
+REM pause
